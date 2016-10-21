32008792273abfb07dc3a8f5453b5f5f2baebf6f
Paul <spread2009@gmail.com>
2016-10-21 22:18:52 +0400

    git add .!

---
+++ src/app/todo-app/todo-app.component.ts
@@ -0,0 +1,38 @@
+import { Component  } from '@angular/core';
+import { Todo } from '../todo';
+import { TodoService } from '../todo.service';
+
+@Component({
+  //moduleId: module.id,
+  selector: 'app-todo',
+  templateUrl: 'todo-app.component.html',
+  styleUrls: ['todo-app.component.css'],
+  providers: [ TodoService ]
+})
+export class TodoAppComponent    {
+
+ newTodo: Todo = new Todo();
+
+  constructor( private todoService: TodoService ) {}
+
+  toggleTodoComplete( todo )
+  {
+    this.todoService.toggleTodoComplete( todo );
+  }
+
+  addTodo()
+  {
+    this.todoService.addTodo( this.newTodo );
+    this.newTodo = new Todo();
+  }
+
+  removeTodo( todo )
+  {
+    this.todoService.deleteTodoById( todo.id );
+  }
+
+  get todos()
+  {
+    return this.todoService.getAllTodos();
+  }
+}
